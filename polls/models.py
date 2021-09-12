2e9f2a698e1f32c50c86af8c579ab9227c0bab38
borrabeam <bannakorn.s@ku.th>
2021-09-12 21:58:04 +0700

    Create is_published and can_vote methods

--- polls/models.py
+++ polls/models.py
@@ -16,6 +16,15 @@ class Question(models.Model):
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    def is_published(self):
+        now = timezone.now()
+        return self.pub_date <= now
+    
+    def can_vote(self):
+        now = timezone.now()
+        return now <= self.end_date and self.is_published()
+
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
