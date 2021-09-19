bcc77f81d405d0c796a67930acd5b157ceacd9c6
borrabeam <bannakorn.s@ku.th>
2021-09-20 01:58:20 +0700

    Externalize configuration data

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,7 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os 
+
 from pathlib import Path
+from decouple import config
+import environ
+
+
+env = environ.Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +28,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6izn4#3za@!k6v!8lwelmav2v!6t3)k$bj)w7f1a^g^c@c*a13'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast= bool)
 
 ALLOWED_HOSTS = []
 
@@ -105,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Bangkok'
+TIME_ZONE = config('TIME_ZONE')
 
 USE_I18N = True
 
