2afa25d4c7060f40ef9d8edc10627be3d4ba2167
borrabeam <bannakorn.s@ku.th>
2021-09-19 22:46:13 +0700

    Create unit test for is_published and can_vote method.

--- polls/tests.py
+++ polls/tests.py
@@ -37,6 +37,63 @@ class QuestionModelTests(TestCase):
         time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=time)
         self.assertIs(recent_question.was_published_recently(), True)
+    
+    def test_is_published_with_past_question(self):
+        """
+        is_published() return False for questions whose pub_date is older than 7 days.
+        """
+        time = timezone.now() - datetime.timedelta(days=7)
+        end_time = time 
+        past_question = Question(pub_date=time,end_date=end_time)
+        self.assertIs(past_question.is_published(), True)
+    
+    def test_is_published_with_recent_question(self):
+        """
+        is_published() return True for question whose pub_date is not within 2 hours.
+        """
+        time = timezone.now() - datetime.timedelta(hours=2)
+        end_time = time + datetime.timedelta(days=1)
+        recent_question = Question(pub_date=time,end_date=end_time)
+        self.assertIs(recent_question.is_published(), True)
+
+    def test_is_published_with_future_question(self):
+        """
+        is_published() return False for questions whose pub_date is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.is_published(), False)
+    
+    def test_can_vote_with_past_question(self):
+        """
+        can_vote() return False for question pub_date is older than end_date
+        """
+
+        time = timezone.now()  - datetime.timedelta(days=7)
+        end_time = time 
+        past_question = Question(pub_date=time,end_date=end_time)
+        self.assertIs(past_question.can_vote(), False)
+
+    
+    def test_can_vote_with_recent_question(self):
+        """
+        can_vote() return True for question pub_date is in time forvote
+        """
+        time = timezone.now() - datetime.timedelta(hours=2)
+        end_time = time + datetime.timedelta(days=1)
+        recent_question = Question(pub_date=time,end_date=end_time)
+        self.assertIs(recent_question.can_vote(), True)
+
+    def test_can_vote_with_future_question(self):
+        """
+        can_vote() return False for whose pub_date is in the future.
+        """
+
+        time = timezone.now() + datetime.timedelta(days=30)
+        end_time = time - date.timedelta(days=1)
+        future_question = Question(pub_date=time, end_date=end_time)
+        self.assertIs(future_question.can_vote(), False)
+    
 
     
 def create_question(question_text, days):
