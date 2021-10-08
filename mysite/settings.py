21cb04069490a6948ec4572c2a8550e26fcf3356
borrabeam <bannakorn.s@ku.th>
2021-10-08 22:29:33 +0700

    update setting and testing travis

--- mysite/settings.py
+++ mysite/settings.py
@@ -14,10 +14,6 @@ from pathlib import Path
 from decouple import config
 import environ
 
-
-env = environ.Env()
-env.read_env()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
